f75ab7ce105210a41b33fb949c24349f9f85836d
phanikumar-ravilisetti <kumarsfine1942@gmail.com>
2025-09-26 13:55:01 +0530

    Update mongo uri

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(cors({
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/mydb");
+    await mongoose.connect(process.env.MONGO_URI)
+
 
     console.log("MongoDB Connected");
 
